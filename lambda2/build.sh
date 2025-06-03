962acc07ac5e1debc3f8b3c271149e07d03423b5
elbaz <eranelbaz97+github@gmail.com>
2025-06-03 14:42:52 +0300

    fix build for libcrypto.so.10

--- lambda2/build.sh
+++ lambda2/build.sh
@@ -5,7 +5,11 @@
 rm layer.zip
 
 docker run --rm -v "$PWD":/tmp/layer lambci/yumda:2 bash -c "
-  yum install -y git-${GIT_VERSION} && \
+  yum install -y git-${GIT_VERSION} openssl-libs openssl-devel && \
   cd /lambda/opt && \
+  # Copy required OpenSSL libraries
+  mkdir -p lib && \
+  cp -a /usr/lib64/libcrypto.so* lib/ && \
+  cp -a /usr/lib64/libssl.so* lib/ && \
   zip -yr /tmp/layer/layer.zip .
 "
